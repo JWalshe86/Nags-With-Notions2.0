eba798fe36607a3a270efb6f04828e5beadbb768
John Walshe <walshejohnnyw7@gmail.com>
2023-11-27 19:25:25 +0000

    chore: move script underneath CDN

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
+//make jquery functions available after the document has loaded
+$(document).ready(function () {
+    // $('h2').css('text-decoration', 'underline'); //underlines all h2 elements
+
+});
+
+
+
 console.log('hello');
