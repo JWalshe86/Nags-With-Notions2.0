5579406ee7d67e593aa0e798b1dbd414290f1e45
John Walshe <walshejohnnyw7@gmail.com>
2023-12-21 10:01:18 +0000

    index.html runs on django

--- nags_with_notions/settings.py
+++ nags_with_notions/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-l(41ts68(20gmp-o8=)kmpqqr)nl-!&a*bfayt$mfify7ec45#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost:8000']
 
 
 # Application definition
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'nags_with_notions.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR), "pizza_system/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
