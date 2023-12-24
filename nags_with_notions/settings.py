5b6a28f0f97b3768f3eec253f7200464abac80fd
John Walshe <walshejohnnyw7@gmail.com>
2023-12-24 10:25:46 +0000

    add Procfile

--- nags_with_notions/settings.py
+++ nags_with_notions/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l(41ts68(20gmp-o8=)kmpqqr)nl-!&a*bfayt$mfify7ec45#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost:8000/']
 
