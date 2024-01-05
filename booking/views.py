50565f0220a5d4e1d4ba78c9ecc9d1d100e8096c
John Walshe <walshejohnnyw7@gmail.com>
2024-01-05 13:34:07 +0000

    booking entry displaying on homepage

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Booking
 
 # Create your views here.
+
+class BookingList(generic.ListView):
+    model = Booking
--- pizza_system/views.py
+++ pizza_system/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
 def myview(request):
