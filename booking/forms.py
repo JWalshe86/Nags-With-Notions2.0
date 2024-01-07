6edc52945c895c7511f74136b6689ea8d943e737
John Walshe <walshejohnnyw7@gmail.com>
2024-01-07 14:33:27 +0000

    adds bootstrap form-control to booking form

--- booking/forms.py
+++ booking/forms.py
@@ -6,4 +6,13 @@ from .models import Booking
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
-        fields = ("name", "phone", "booking_date", "guest_number", "status")
+        fields = ("name", "phone", "booking_date", "guest_number", "message", "status")
+        
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control'}),
+            'booking_date': forms.TextInput(attrs={'class': 'form-control'}),
+            'guest_number': forms.TextInput(attrs={'class': 'form-control'}),
+            'message': forms.TextInput(attrs={'class': 'form-control'}),
+            'status': forms.TextInput(attrs={'class': 'form-control'}),
+        }
