212ab0015b51f58d5d7751a1bc104fa2da04e902
John Walshe <walshejohnnyw7@gmail.com>
2024-01-07 14:48:19 +0000

    add bootstrap placeholder txt booking form

--- booking/forms.py
+++ booking/forms.py
@@ -7,12 +7,20 @@ class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ("name", "phone", "booking_date", "guest_number", "message", "status")
+        labels = {
+            'name': '',
+            'phone': '',
+            'booking_date': '',
+            'guest_number': '',
+            'message': '',
+            'status': '',
+        }
         
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control'}),
-            'phone': forms.TextInput(attrs={'class': 'form-control'}),
-            'booking_date': forms.TextInput(attrs={'class': 'form-control'}),
-            'guest_number': forms.TextInput(attrs={'class': 'form-control'}),
-            'message': forms.TextInput(attrs={'class': 'form-control'}),
-            'status': forms.TextInput(attrs={'class': 'form-control'}),
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Name'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter phone number'}),
+            'booking_date': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter booking date'}),
+            'guest_number': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter guest numbers'}),
+            'message': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Message'}),
+            'status': forms.TextInput(attrs={'class': 'form-control', 'placeholder': ''}),
         }
