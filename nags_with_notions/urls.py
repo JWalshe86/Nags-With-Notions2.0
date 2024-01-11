4ee994ce8b353200e5427253ca7f34015cb0e68b
John Walshe <walshejohnnyw7@gmail.com>
2024-01-11 17:24:07 +0000

    summernote content works

--- nags_with_notions/urls.py
+++ nags_with_notions/urls.py
@@ -19,10 +19,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('pizza_system.urls')),
-    path('summernote/', include('summernote.urls')),
+    path('summernote/', include('django_summernote.urls')),
     path("accounts/", include("allauth.urls")),
     path("events/", include("events.urls"), name="events-urls"),
     path('', include('booking.urls')),
     path('admin/', admin.site.urls),
-    path('summernote/', include('summernote.urls')),
 ]
