c8e9f8277c400ec2b26993111d1264e3eadb6f6b
John Walshe <walshejohnnyw7@gmail.com>
2024-01-29 12:01:45 +0000

    add styling to events display page

--- events/forms.py
+++ events/forms.py
@@ -5,12 +5,5 @@ from .models import Event
 class EventForm(forms.ModelForm):
     class Meta:
         model = Event
-        fields = ('title', 'slug', 'author', 'content', 'event_image', 'status')
+        fields = ('author', 'title', 'slug', 'content', 'event_image', 'status')
                 
-        widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Please enter event title'}),
-            'slug': forms.TextInput(attrs={'class': 'form-control'}),
-            'author': forms.Select(attrs={'class': 'form-control'}),
-            'content': forms.Textarea(attrs={'class': 'form-control'}),
-            'status': forms.NumberInput(attrs={'class': 'form-control'}),
-        }
--- events/models.py
+++ events/models.py
@@ -5,11 +5,9 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
 class Event(models.Model):
+    author =  models.CharField(max_length=200, unique=True)
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(
-    User, on_delete=models.CASCADE, related_name="events"   
-    )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
