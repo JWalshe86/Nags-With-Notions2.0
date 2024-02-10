c5ba780baeee1967e6b7549c8366e1dd84e6ccfc
John Walshe <walshejohnnyw7@gmail.com>
2024-02-10 00:03:34 +0000

    removes name form booking model

--- booking/models.py
+++ booking/models.py
@@ -5,7 +5,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
 class Booking(models.Model):
     user = models.ForeignKey(User, default=1, on_delete=models.CASCADE, null=True, related_name='booking')
-    name = models.CharField(max_length=200)
     booking_date = models.CharField(max_length=200)
     location = models.CharField(max_length=150)
     guest_number = models.IntegerField(default=10)
