c3b605c356f9eec8a4ad6a05b0f24a08330f6552
John Walshe <walshejohnnyw7@gmail.com>
2024-02-10 09:41:12 +0000

    bookings order by newest created on top of list

--- booking/models.py
+++ booking/models.py
@@ -4,16 +4,19 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 class Booking(models.Model):
-    user = models.ForeignKey(User, default=1, on_delete=models.CASCADE, null=True, related_name='booking')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1, null=True, blank=True, related_name='booking')
     booking_date = models.CharField(max_length=200)
     location = models.CharField(max_length=150)
     guest_number = models.IntegerField(default=10)
-    message = models.CharField()
-    created_on = models.DateTimeField(auto_now=True)
+    message = models.TextField(null=True, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
 
     
     def __str__(self):
         return self.message
+    
+    class Meta:
+        ordering = ['-created_on']
     
 
     
