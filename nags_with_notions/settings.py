5633dfe0d5a4308dec17708effd700c07c0dbbcb
John Walshe <walshejohnnyw7@gmail.com>
2024-03-09 18:50:54 +0000

    add all env variables to heroku config vars

--- nags_with_notions/settings.py
+++ nags_with_notions/settings.py
@@ -35,10 +35,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
-print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG') == True
 
 ALLOWED_HOSTS = ['*']
 
@@ -111,7 +110,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', 
         'NAME': 'bxavbsbh',                     
         'USER': 'bxavbsbh',
-        'PASSWORD': 'BCi1AkMTZQzBfA0cyktpuMTwD5lzNrn9',
+        'PASSWORD': os.environ.get('PASSWORD'),
         'HOST': 'mel.db.elephantsql.com', 
         'PORT': '',              
     }
