18e18e769c20293d8d7b976cfdc6a0ada4963324
John Walshe <walshejohnnyw7@gmail.com>
2024-06-25 20:40:31 +0100

    Add secret variable for db password

--- nags_with_notions/settings.py
+++ nags_with_notions/settings.py
@@ -115,7 +115,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "nwn",
         "USER": "johnwalshe",
-        "PASSWORD": "Sunshine7!",
+        "PASSWORD": os.environ.get('DB-PASSWORD'),
         "HOST": "localhost",
         "PORT": "5432",
     }
