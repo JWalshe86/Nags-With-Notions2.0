6a24156dde00cc84ac9290262986172fcb9439f5
John Walshe <walshejohnnyw7@gmail.com>
2024-06-26 16:47:12 +0100

    Adds db config settings.py

--- nags_with_notions/settings.py
+++ nags_with_notions/settings.py
@@ -17,7 +17,6 @@ import dj_database_url
 
 load_dotenv()
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
@@ -109,6 +108,8 @@ WSGI_APPLICATION = "nags_with_notions.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+if "DATABASE_URL" in os.environ:
+        DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 
 DATABASES = {
     "default": {
