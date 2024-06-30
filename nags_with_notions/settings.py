72ffe0cf66f34d6dc2a172d76afa21e71d212869
John Walshe <walshejohnnyw7@gmail.com>
2024-06-30 23:35:59 +0100

    :pencil2: Change bucket name to lower case

--- nags_with_notions/settings.py
+++ nags_with_notions/settings.py
@@ -32,7 +32,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 if 'USE_AWS' in os.environ:
-    AWS_STORAGE_BUCKET_NAME = 'Nags-With-Notions2.0'
+    AWS_STORAGE_BUCKET_NAME = 'nags-with-notions2.0'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
