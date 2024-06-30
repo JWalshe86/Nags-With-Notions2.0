de3ab65ff726be46f2b1926305ed01b520c0dc72
John Walshe <walshejohnnyw7@gmail.com>
2024-06-30 22:06:46 +0100

    Edit AWS settings - incorrect variable name

--- nags_with_notions/settings.py
+++ nags_with_notions/settings.py
@@ -36,7 +36,7 @@ if 'USE_AWS' in os.environ:
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AW3_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
