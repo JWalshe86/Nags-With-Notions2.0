71b94d8a6ae12a16bc16f7c81a7e6e1a2a6f7b89
John Walshe <walshejohnnyw7@gmail.com>
2024-08-30 22:53:35 +0100

    correct home url

--- pizza_system/urls.py
+++ pizza_system/urls.py
@@ -12,9 +12,7 @@ urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('register/', RegisterPage.as_view(), name='register'),
+    path('', views.index_view, name='home'),
 ]
 
 
-# urlpatterns = [
-#     path('', views.myview, name='home'),
-# ]
