e756d9185b7ad082ef6e6799d561008118dce3b0
John Walshe <walshejohnnyw7@gmail.com>
2024-09-05 21:35:13 +0100

    remove media from MEDIA_URL path

--- nags_with_notions/settings.py
+++ nags_with_notions/settings.py
@@ -33,7 +33,7 @@ if USE_AWS:
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com"
 
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
-    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
+    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
     
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
