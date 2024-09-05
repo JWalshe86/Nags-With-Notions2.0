1917075222c35db0675caa4c1a990589bad663e6
John Walshe <walshejohnnyw7@gmail.com>
2024-09-05 22:04:16 +0100

    Remove events from path

--- events/models.py
+++ events/models.py
@@ -4,7 +4,7 @@ class Event(models.Model):
     "creates event model"
     name = models.CharField(max_length=200)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(upload_to='events/images/', null=True, blank=True)  # Added upload_to
+    image = models.ImageField(upload_to='images/', null=True, blank=True)  # Added upload_to
     description = models.TextField(max_length=1024, null=True, blank=True)
     start_date = models.DateTimeField()
 
