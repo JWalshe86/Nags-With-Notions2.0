f6770ed9c5011e04a830491a1374c360816f5d70
John Walshe <walshejohnnyw7@gmail.com>
2024-09-05 21:45:01 +0100

    Update events image model

--- events/models.py
+++ events/models.py
@@ -1,15 +1,14 @@
-"imports"
-from django.db import models
-
+from django.db import models  # Ensure this import is present
 
 class Event(models.Model):
     "creates event model"
     name = models.CharField(max_length=200)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(upload_to='events/images/', null=True, blank=True)  # Added upload_to
     description = models.TextField(max_length=1024, null=True, blank=True)
     start_date = models.DateTimeField()
 
     def __str__(self):
         "returns name"
         return f"{self.name}"
+
