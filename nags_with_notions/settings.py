3b682217cff9375718b858a9aaaf5e908d0a8c1e
John Walshe <walshejohnnyw7@gmail.com>
2024-09-06 11:33:14 +0100

    Update S3 url

--- nags_with_notions/settings.py
+++ nags_with_notions/settings.py
@@ -55,12 +55,12 @@ if USE_AWS:
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com"
 
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
-    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
+    MEDIA_URL = os.getenv('CLOUDFRONT_URL', f"https://{AWS_S3_CUSTOM_DOMAIN}/")
 
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     
-    S3_BASE_URL = os.getenv('S3_BASE_URL', MEDIA_URL)
+    S3_BASE_URL = os.getenv('CLOUDFRONT_URL', MEDIA_URL)
     logger.debug(f"S3_BASE_URL set to: {S3_BASE_URL}")
 else:
     STATIC_URL = '/static/'
